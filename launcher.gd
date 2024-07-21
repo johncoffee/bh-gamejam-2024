bddf8d23de42328840bbafef098670e3d49b7ef7
julian <julian@triband.net>
2024-07-21 17:50:18 +0200

    this works! sound and everything

--- launcher.gd
+++ launcher.gd
@@ -4,10 +4,15 @@ extends Node3D
 @onready var toaster:Node3D = $toaster as Node3D
 @onready var plate:Node3D = $plate as Node3D
 
+@onready var camOrigin:Vector3 = $Camera3D.position
+
 @export_range(0, 10, 0.1) var launchSpeedX: float
 @export_range(0, 10, 0.1) var launchSpeedY: float
 
 var won = false
+
+func _ready():
+	pass
 
 func _process(delta):
 	if Input.is_action_just_pressed("shoot"):
@@ -17,12 +22,24 @@ func _process(delta):
 	if !won && toastBread.sleeping && toastBread.position.distance_to(plate.position) < Vector3(1.5, 0, 0).length():
 		won = true
 		on_win()
-
+		
+	#shake
+	if won:
+		$Camera3D.position = camOrigin + Vector3( randf() * 0.1, randf() * 0.1, 0 )
+	
 func on_win():
-	print("Won!")
-
+	($sounds/jazz as AudioStreamPlayer).stop()
+	($sounds/metal as AudioStreamPlayer).play()
+	$ui/RichTextLabel.visible = true
+	
 func launchToast():
+	($"sounds/toast shoot" as AudioStreamPlayer).play()
 	toastBread.linear_velocity = Vector3()
-	toastBread.angular_velocity = Vector3()	
+	toastBread.angular_velocity = Vector3(0, 0, -1)	
 	toastBread.position = toaster.position
 	toastBread.apply_central_impulse(Vector3(launchSpeedX,launchSpeedY,0))
+
+
+func _on_toast_body_entered(body):
+	$sounds/impact.play()
+	
--- should-collide.gd
+++ should-collide.gd
@@ -1,13 +1,15 @@
 extends RigidBody3D
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass	
+	pass
 
-func body_entered(body):
-	print("ouch 1 ")
+func _on_body_entered(body):
+	print(42)
 	
