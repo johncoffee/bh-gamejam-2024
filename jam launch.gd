e832d9cb9e1d07b2666e49ab8c5107c4aced112a
--global <--global>
2024-07-22 13:52:05 +0200

    game loop with jam

--- jam launch.gd
+++ jam launch.gd
@@ -1,19 +1,3 @@
 extends Node3D
 
-@export_range(5, 20, 0.5) var launchSpeedY: float
-
-@onready var projectile:Node3D = $Node3D/projectile as Node3D
-
-func _ready():
-	pass # Replace with function body.
-
-func _process(delta):
-	if Input.is_action_just_pressed("shoot"):
-		launchIt()
-
-func launchIt():
-	projectile.angular_velocity = Vector3(0, 0, 0)
-	projectile.linear_velocity = Vector3(0, 0, 0)	
-	projectile.position = $Node3D/CSGBox3D.position
-	projectile.apply_central_impulse(Vector3(0,launchSpeedY,0))
 
--- launcher.gd
+++ launcher.gd
@@ -9,16 +9,48 @@ extends Node3D
 @export_range(0, 10, 0.1) var launchSpeedX: float
 @export_range(0, 10, 0.1) var launchSpeedY: float
 
+@export_range(5, 25, 0.25) var launchJamSpeedY: float
+
+@onready var projectile:Node3D = $jam/projectile
+
+#func _process(delta):
+	#if Input.is_action_just_pressed("shoot"):
+		#launchIt()
+
 var won = false
+var toastLaunched = false
+var jamLaunched = false
 
 func _ready():
 	pass
 
 func _process(delta):
-	if Input.is_action_just_pressed("shoot"):
-		launchToast()
+	if Input.is_action_just_pressed("resetter"):
+		if get_tree():
+			get_tree().reload_current_scene()
+		return
 		
-	# detect win
+	if Input.is_action_just_pressed("shoot"):
+		if !toastLaunched:
+			toastLaunched = true
+			launchToast()
+		elif !jamLaunched:
+			jamLaunched = true
+			launchIt()
+		elif jamLaunched && toastLaunched:
+			# reset toast
+			toastBread.linear_velocity = Vector3()
+			toastBread.angular_velocity = Vector3(0, 0, 0)	
+			toastBread.position = toaster.position
+			# reset jam
+			projectile.angular_velocity = Vector3(0, 0, 0)
+			projectile.linear_velocity = Vector3(0, 0, 0)	
+			projectile.position = $jam.position
+			
+			toastLaunched = false
+			jamLaunched = false
+		
+		# detect win
 	if !won && toastBread.sleeping && toastBread.position.distance_to(plate.position) < Vector3(1.5, 0, 0).length():
 		won = true
 		on_win()
@@ -38,8 +70,18 @@ func launchToast():
 	toastBread.angular_velocity = Vector3(0, 0, -1)	
 	toastBread.position = toaster.position
 	toastBread.apply_central_impulse(Vector3(launchSpeedX,launchSpeedY,0))
-
+	
+	# reset jam
+	projectile.angular_velocity = Vector3(0, 0, 0)
+	projectile.linear_velocity = Vector3(0, 0, 0)	
+	projectile.position = $jam.position
 
 func _on_toast_body_entered(body):
 	$sounds/impact.play()
+	
+func launchIt():
+	projectile.angular_velocity = Vector3(0, 0, 0)
+	projectile.linear_velocity = Vector3(0, 0, 0)	
+	projectile.position = $jam.position
+	projectile.apply_central_impulse(Vector3(0,launchJamSpeedY,0))
 	
